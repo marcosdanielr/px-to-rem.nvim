b171d6bc4b58424d15cebf91c3d0478500149bb7
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-22 15:45:33 -0300

    CHORE: calc functions return string

--- px-to-rem/lua/utils/calc.lua
+++ px-to-rem/lua/utils/calc.lua
@@ -1,14 +1,20 @@
+local only_numbers = require('only_numbers')
+
 local M = {}
 
-function M.px_to_rem(px)
+function M.px_to_rem(px_value)
   local rem = 16
-  local result = px / rem
+  local px = only_numbers(px_value, 'px')
+  local converted = px / rem
+  local result = converted .. 'rem'
 
   return result
 end
 
-function M.rem_to_px(rem)
-  local result = rem * 16
+function M.rem_to_px(rem_value)
+  local rem = only_numbers(rem_value, 'rem')
+  local converted = rem * 16
+  local result = converted .. 'px'
 
   return result
 end
